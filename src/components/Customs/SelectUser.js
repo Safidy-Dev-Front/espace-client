6c138db86ab2e6ef87d03f0a39e71e2cf2bab9e5
Safidi <safidiniainaromino13@gmail.com>
2022-09-13 22:16:17 +0300

    feat: style select component

--- src/components/Customs/SelectUser.js
+++ src/components/Customs/SelectUser.js
@@ -1,23 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 const SelectUser = ()=>{
     const BASE_URL = window.location.origin;
     //State users===============
     const [users, setUsers] = useState([]);
     const [usersSelected, setUsersSelected ] = useState({});
+    //Class ====================
+    const [class_ul, setClass_ul] = useState(false);
+
+    //Ref=======================
+    const client_wrapper = useRef(null)
     useEffect(() => {
         axios.get(`${BASE_URL}/wp-json/api/v1/users`)
             .then((response) => {
                 setUsers(response.data)
             }).catch((error) => console.error(`Error:${error}`))
     }, []);
-    const onSelectClient = (e) =>{
-        const user_selected = users.find((u)=>u.ID === e.target.getAttribute('value')) 
+    const onToggleClient = ()=>{
+        setClass_ul(!class_ul)    
+    }
+    const onSelectClient = (id) =>{
+        const user_selected = users.find((u)=>u.ID === id) 
         setUsersSelected(user_selected);
+        setClass_ul(false)    
+
     }
     return(
         <div className="container__select-client">
-            <div className="select-client__selected">
+            <div className="select-client__selected" onClick={onToggleClient}>
                 {
                     usersSelected.display_name ? 
                     <div className="client_selected_item d-flex align-items-center">
@@ -39,19 +49,19 @@ const SelectUser = ()=>{
                     </div>
                 }
             </div>
-            <ul className="select-client__wrapper">
+            <ul ref={client_wrapper} className={class_ul?"select-client__wrapper isShow": "select-client__wrapper"}>
                 {
                  users.map((user, index)=>{
-                        return <li onClick={onSelectClient} key={index} value={user.ID}>
-                                <div className="d-flex  select-client__item" value={user.ID}>
-                                    <div className="select-client__image" value={user.ID}>
-                                        <img src={user.image} value={user.ID}/>
+                        return <li onClick={()=>onSelectClient(user.ID)} key={index} >
+                                <div className="d-flex  select-client__item" >
+                                    <div className="select-client__image" >
+                                        <img src={user.image} />
                                     </div>
-                                    <div className="select-client__name-job" value={user.ID}>
-                                        <p className="select-client__name" value={user.ID}>
+                                    <div className="select-client__name-job" >
+                                        <p className="select-client__name" >
                                             {user.display_name}
                                         </p>
-                                        <p className="select-client__company" value={user.ID}>
+                                        <p className="select-client__company" >
                                             {user.company}
                                         </p>
                                     </div>
